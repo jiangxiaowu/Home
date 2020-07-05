a2389111de21ef7c1e0cd888fe11f2bb4479bf79
jiangxiaowu <jiangxiaowu6868@gmail.com>
2020-07-05 23:10:09 +0800

    'init'

---
+++ src/views/Music.vue
@@ -0,0 +1,14 @@
+<template>
+  <Player></Player>
+</template>
+
+<script>
+import Player from "@/components/music/Player.vue";
+
+export default {
+  name: "Music",
+  components: {
+    Player
+  }
+};
+</script>
